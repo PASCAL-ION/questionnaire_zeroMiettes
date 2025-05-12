f624a8ac6712a3d51a5ac717e0c59c9408d1ae6c
pascal ion <ion.pascal@epitech.eu>
2025-05-12 16:28:28 +0200

    deployment test

--- app/test-technique/admin/login/page.tsx
+++ app/test-technique/admin/login/page.tsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
         className="bg-white p-6 rounded shadow-md w-full max-w-sm"
       >
         <h1 className="text-2xl font-bold mb-4">Connexion Admin</h1>
-        {error && <p className="text-red-500 mb-4">{error}</p>}
+        {error && <p className="text-red-600 mb-4">{error}</p>}
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1">Email</label>
           <input
